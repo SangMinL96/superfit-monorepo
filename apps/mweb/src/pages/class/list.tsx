47faee090209535691aa4267c63ce12953ab3692
SangMinL96 <hwon3794@gmail.com>
2025-03-24 15:17:34 +0900

    🚀 수업관련 ui작업

---
+++ apps/mweb/src/pages/class/list.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import cx from 'clsx';
+import styles from '@src/styles/class/list.module.scss';
+import Svgs from '@superfit/design/Svgs';
+import { Button } from '@superfit/design/button';
+import Wrap from '@superfit/design/wrap';
+import Divide from '@superfit/design/Divide';
+import { useRouter } from 'next/router';
+
+function Index() {
+    const router = useRouter();
+    return (
+        <div className={cx(styles.wrap)}>
+            <h3 className={cx(styles.tit)}>수업 관리</h3>
+            <Wrap padding='0 20px' direction='row' justify='space-between' align='flex-end'>
+                <p className={cx(styles.desc)}>
+                    - 수업이름,수업장소,층수 포함하여 생성 해주세요
+                    <br />- 2명 이상의 선생님 수업이 있을 경우 그룹 관리 됩니다
+                </p>
+                <Button
+                    type='button'
+                    width='50px'
+                    fontSize='1.2rem'
+                    size={50}
+                    onClick={() => router.push('/form/classcreate')}
+                >
+                    수업생성
+                </Button>
+            </Wrap>
+            <Divide value={10} margin='30px 0 0' />
+            <Wrap margin='10px 0 0' padding='0 20px'>
+                <h5 className={cx(styles.group_tit)}>이상민 강사</h5>
+                <ul className={cx(styles.class_list)}>
+                    <li>
+                        <button type='button' className={cx(styles.btn)}>
+                            <span className={cx(styles.txt_box)}>
+                                <Svgs name='messageEdit' />
+                                <em className={cx(styles.txt)}>그룹수업</em>
+                            </span>
+                            <Svgs name='arrowRight' />
+                        </button>
+                    </li>
+                    <li>
+                        <button type='button' className={cx(styles.btn)}>
+                            <span className={cx(styles.txt_box)}>
+                                <Svgs name='messageEdit' />
+                                <em className={cx(styles.txt)}>그룹수업</em>
+                            </span>
+                            <Svgs name='arrowRight' />
+                        </button>
+                    </li>
+                </ul>
+                <h5 className={cx(styles.group_tit)}>이다슬 강사</h5>
+                <ul className={cx(styles.class_list)}>
+                    <li>
+                        <button type='button' className={cx(styles.btn)}>
+                            <span className={cx(styles.txt_box)}>
+                                <Svgs name='messageEdit' />
+                                <em className={cx(styles.txt)}>그룹수업</em>
+                            </span>
+                            <Svgs name='arrowRight' />
+                        </button>
+                    </li>
+                    <li>
+                        <button type='button' className={cx(styles.btn)}>
+                            <span className={cx(styles.txt_box)}>
+                                <Svgs name='messageEdit' />
+                                <em className={cx(styles.txt)}>그룹수업</em>
+                            </span>
+                            <Svgs name='arrowRight' />
+                        </button>
+                    </li>
+                </ul>
+            </Wrap>
+        </div>
+    );
+}
+
+export default Index;
--- packages/design/src/components/button/Button.tsx
+++ packages/design/src/components/button/Button.tsx
@@ -2,28 +2,41 @@ import { clsx } from 'clsx';
 import styles from './Button.module.scss';
 import { ReactNode } from 'react';
 type Props = {
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  disabled?: boolean;
-  name: string;
-  size?: 64 | 56 | 50 | 44 | 40 | 36 | 32;
-  width?: string;
-  type: 'button' | 'reset' | 'submit';
-  children: ReactNode;
-  color?: 'primary' | 'outlined' | 'light' | 'grey' | 'shadow';
-  margin?: string;
-  padding?: string;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    disabled?: boolean;
+    name?: string;
+    size?: 64 | 56 | 50 | 44 | 40 | 36 | 32;
+    width?: string;
+    type: 'button' | 'reset' | 'submit';
+    children: ReactNode;
+    color?: 'primary' | 'outlined' | 'light' | 'grey' | 'shadow';
+    margin?: string;
+    fontSize?: string;
+    padding?: string;
 };
-export const Button = ({ width, name, type, children, color = 'primary', disabled, onClick, size = 50, margin, padding }: Props) => {
-  return (
-    <button
-      style={{ width, margin, padding }}
-      type={type}
-      name={name}
-      className={clsx(styles.wrap, styles[`size_${size}`], styles[color])}
-      disabled={disabled}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
+export const Button = ({
+    width,
+    name,
+    type,
+    children,
+    fontSize,
+    color = 'primary',
+    disabled,
+    onClick,
+    size = 50,
+    margin,
+    padding,
+}: Props) => {
+    return (
+        <button
+            style={{ width, margin, padding, fontSize }}
+            type={type}
+            name={name}
+            className={clsx(styles.wrap, styles[`size_${size}`], styles[color])}
+            disabled={disabled}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
 };
